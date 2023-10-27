9a9066222c5d4b21d5856ae9d31a2ce90646274d
Jacob Gabbert <jacobgabbert3@gmail.com>
2023-10-27 08:28:33 -0500

    Added section 5 part 38 of udemy cpp

---
+++ udemy/cpp/section_5/part_38/preprocessor_directives.cpp
@@ -0,0 +1,16 @@
+// The C++ Preprocessor is a program that processes source code before it gets to the compiler,
+// it then strips the program of all comments and replaces them with a single whitespace character,
+// it then looks to execute the 'preprocessor directives' which all begin with a '#' symbol.
+//
+// The preprocessor DOES NOT undestand C++ source code.
+
+// preprocessor directives
+#include <iostream>
+
+// function main
+int main() {
+      // display a message to the user
+      std::cout << "Hello, World!\n";
+
+      return 0;
+}
