ac03d286ed18b6b390f41e7e8709081e6c8a8e18
Jacob Gabbert <jacobgabbert3@gmail.com>
2023-10-27 08:33:03 -0500

    Added section 5 part 39 of udemy cpp

---
+++ udemy/cpp/section_5/part_39/comments.cpp
@@ -0,0 +1,22 @@
+// Comments are annotations that will not be read by the compiler as they are removed by the preprocessor.
+//
+// Comments are removed before compilation and replaced by a single whitespace character.
+//
+// They can be written to ways:
+
+// Like this
+
+/*
+or like this
+*/
+
+// preprocessor directives
+#include <iostream>
+
+// function main
+int main() {
+      // display a message to the user
+      std::cout << "Hello, World!\n";
+
+      return 0;
+}
