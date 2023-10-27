0dbea1381b90944893b6acab69f863dee212f57d
Jacob Gabbert <jacobgabbert3@gmail.com>
2023-10-26 20:50:53 -0500

    Added section 4 part 31 of udemy cpp

---
+++ udemy/cpp/section_4/part_31/runtime_errors.cpp
@@ -0,0 +1,12 @@
+// Runtime errors occur when the program is executed and is running.
+// i.e. dividing by zero, file not found, out of memory, etc.
+
+// preprocessor directives
+#include <iostream>
+
+// function main
+int main() {
+
+
+      return 0;
+}
