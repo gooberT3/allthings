9c76e143f1d3ea949587c8786ef8f9202ef7b92b
Jacob Gabbert <jacobgabbert3@gmail.com>
2023-10-27 08:21:34 -0500

    Added section 5 part 37 of udemy cpp

---
+++ udemy/cpp/section_5/structure.cpp
@@ -0,0 +1,21 @@
+// Identifiers, not standard keywords, created by programmers to give something meaningful a name
+// i.e. #include, main, cout
+//
+// Operators, these are characters that perform operations when used
+// i.e +, -, *, /, <<, >>
+//
+// Punctuation, these are certain characters used to perform other actions
+// i.e. ;, (), {}
+//
+// Combined all these parts mesh together to create the programming language syntax
+
+// preprocessor directives
+#include <iostream>
+
+// function main
+int main() {
+      // display 'Hello, World!' to the user
+      std::cout << "Hello, World!\n";
+
+      return 0;
+}
