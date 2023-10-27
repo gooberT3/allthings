1b1fbcaa39e4d57c1fa6bed3f5af6bca42a3d06d
Jacob Gabbert <jacobgabbert3@gmail.com>
2023-10-26 20:37:59 -0500

    Added section 4 part 29 of udemy cpp

---
+++ udemy/cpp/section_4/part_29/compiler_warnings.cpp
@@ -0,0 +1,15 @@
+// Compiler Warnings are issues within your program that could lead to bigger problems, but is still able to generate proper machine code
+
+// preprocessor directives
+#include <iostream>
+
+// function main
+int main() {
+      // declare an integer variable
+      int foo;
+
+      // display to the user the integer in 'foo'
+      std::cout << "Number is: " << foo;
+
+      return 0;
+}
