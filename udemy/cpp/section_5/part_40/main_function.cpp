4ac904052c4d46f52917f6f74a2354f3f2994ff1
Jacob Gabbert <jacobgabbert3@gmail.com>
2023-10-27 08:42:57 -0500

    Added section 5 part_40 of udemy cpp

---
+++ udemy/cpp/section_5/part_40/main_function.cpp
@@ -0,0 +1,12 @@
+// Every C++ program must have a main function.
+
+// preprocessor directives
+#include <iostream>
+
+// function main
+int main() {
+      // display a message to the user
+      std::cout << "Hello, World!\n";
+
+      return 0;
+}
